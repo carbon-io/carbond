bd81fcb8939c75a39abc2f7c2ad4282ee6e3f008
Greg Banks <gbanks@gmail.com>
2015-10-20 08:15:24 -0700

    remove app-harness

--- test/app-harness.js
+++
@@ -1,9 +0,0 @@
-var ObjectServer = require('../lib/ObjectServer')
-var o  = require('atom').o(module)
-var __ = require('fiber').__(module, true)
-
-__(function() {
-  module.exports = o({
-    _type: ObjectServer
-  })
-})
