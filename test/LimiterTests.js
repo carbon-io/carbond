3d3c663801c76df5304c6eb0201e51ca3284c818
Greg Banks <gbanks@gmail.com>
2016-09-09 11:29:55 -0700

    fixes, more tests

--- test/LimiterTests.js
+++ test/LimiterTests.js
@@ -878,17 +878,19 @@ module.exports = o({
       teardown: function() {
         Service.prototype._init.restore()
       },
-      buildLimiter: function() {
+      buildLimiter: function(visits) {
+        visits = _.isUndefined(visits) ? 1 : visits
         return o({
           _type: limiters.FunctionLimiter,
           _fn: function(req, res) {
             if (!('visits' in this.state)) {
               this.state.visits = 0
             }
-            if (this.state.visits === 1) {
+            if (this.state.visits === visits) {
+              this.sendUnavailable(res)
               return false
             }
-            this.state.visits = 1
+            this.state.visits += 1
             return true
           }
         })
@@ -918,6 +920,82 @@ module.exports = o({
               assert.equal(limiter.service, self.parent.service)
               assert.equal(limiter.node, self.parent.service)
             })
+          }
+        }),
+        o({
+          _type: testtube.Test,
+          name: 'TestLimiterChainInitialize',
+          description: 'Test LimiterChain initialize method',
+          doTest: function() {
+            var self = this
+            var limiters_ = [
+              this.parent.buildLimiter(3),
+              this.parent.buildLimiter(2),
+              this.parent.buildLimiter(1)
+            ]
+            limiters_.forEach(function(limiter) {
+              sinon.stub(limiter, 'sendUnavailable', function() { })
+            })
+            var limiterChain = o({
+              _type: limiters.LimiterChain,
+              limiters: limiters_
+            })
+            limiterChain.initialize(this.parent.service, this.parent.service)
+
+            var next = sinon.spy()
+
+            var reset = function() {
+              next.reset()
+              limiters_.forEach(function(limiter) {
+                limiter.sendUnavailable.reset()
+              })
+            }
+
+            // req passes through
+            limiterChain.process({}, {}, next)
+            assert(next.called)
+            assert.equal(0, next.firstCall.args.length)
+            limiters_.forEach(function(limiter) {
+              assert(!limiter.sendUnavailable.called)
+            })
+            reset()
+
+            // req rejected by last limiter in chain
+            limiterChain.process({}, {}, next)
+            assert(!next.called)
+            assert(!limiters_[0].sendUnavailable.called)
+            assert(!limiters_[1].sendUnavailable.called)
+            assert(limiters_[2].sendUnavailable.called)
+            reset()
+
+            // req rejected by second limiter in chain
+            limiterChain.process({}, {}, next)
+            assert(!next.called)
+            assert(!limiters_[0].sendUnavailable.called)
+            assert(limiters_[1].sendUnavailable.called)
+            assert(!limiters_[2].sendUnavailable.called)
+            reset()
+
+            // req rejected by first limiter in chain
+            limiterChain.process({}, {}, next)
+            assert(!next.called)
+            assert(limiters_[0].sendUnavailable.called)
+            assert(!limiters_[1].sendUnavailable.called)
+            assert(!limiters_[2].sendUnavailable.called)
+            reset()
+
+            sinon.stub(limiters_[0], 'process', function(req, res, next) {
+              next(new Error('foo'))
+            })
+            limiterChain.process({}, {}, next)
+            assert(next.called)
+            assert.equal(next.firstCall.args.length, 1)
+            assert(next.firstCall.args[0] instanceof Error)
+            assert(!limiters_[0].sendUnavailable.called)
+            assert(!limiters_[1].sendUnavailable.called)
+            assert(!limiters_[2].sendUnavailable.called)
+            reset()
+            limiters_[0].process.restore()
           }
         }),
       ]
