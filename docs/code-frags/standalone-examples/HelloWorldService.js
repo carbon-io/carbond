342e1d8f1086f86f6b70121cda455acf8fd6400a
Greg Banks <gbanks@gmail.com>
2017-06-06 17:09:56 -0700

    swap link/files in code-frags

--- docs/code-frags/standalone-examples/HelloWorldService.js
+++ docs/code-frags/standalone-examples/HelloWorldService.js
@@ -1,1 +1,22 @@
-ServiceSimpleEndpointExample.js
+var carbon = require('carbon-io')
+var o  = carbon.atom.o(module)
+var __ = carbon.fibers.__(module)
+
+__(function() {
+  module.exports = o.main({
+    _type: carbon.carbond.Service,
+    port: 8888,
+    endpoints: {
+      // Endpoint definitions go here
+      hello: o({
+        _type: carbon.carbond.Endpoint,
+        get: function(req) {
+          return {msg: "Hello World!"}
+        },
+        post: function(req) {
+          return {msg: req.body.msg}
+        }
+      }) 
+    }
+  }) 
+})
--- docs/code-frags/standalone-examples/MyAcl.js
+++ docs/code-frags/standalone-examples/MyAcl.js
@@ -1,1 +1,46 @@
-ACLExternalACLExample.js
+var carbon = require('carbon-io')
+var o  = carbon.atom.o(module)
+
+module.exports = o({
+  _type: carbon.carbond.security.CollectionAcl,
+  /*
+   * Your ACL definition
+   */
+  groupDefinitions: { // This ACL defined two groups, 'role' and 
+                      // 'title'.
+    role: 'role', // We define a group called 'role' based on the 
+                  // user property named 'role'.
+    title: function(user) { return user.title } 
+  },
+  entries: [
+    {
+      user: { role: 'Admin' },
+      permissions: {
+        '*': true // '*' grants all permissions 
+      }
+    },
+    {
+      user: { title: 'CFO' },
+      permissions: { 
+        find: true,
+        findObject: true,
+        '*': false // This is implied since the default value for 
+                   // all permissions is `false`.
+      }
+    },
+    {
+      user: '10002', // User with _id '10002'
+      permissions: { 
+        insert: true,
+        findObject: true
+      }
+    },
+    {
+      user: '*', // All other users
+      permissions: { 
+        findObject: true
+      }
+    }
+  ]
+})
+
